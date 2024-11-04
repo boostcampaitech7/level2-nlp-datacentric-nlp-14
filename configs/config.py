a277a1adac7c19493adc33459e8b4552bbf7876a
Hyejun An <jagaldol.dev@gmail.com>
2024-11-04 13:31:48 +0900

    fix: 불필요한 path의 `./` 제거

--- configs/config.py
+++ configs/config.py
@@ -5,5 +5,5 @@ import torch
 SEED = 456
 DEVICE = torch.device("cuda") if torch.cuda.is_available() else torch.device("cpu")
 BASE_DIR = os.getcwd()
-DATA_DIR = os.path.join(BASE_DIR, "./data")
-OUTPUT_DIR = os.path.join(BASE_DIR, "./output")
+DATA_DIR = os.path.join(BASE_DIR, "data")
+OUTPUT_DIR = os.path.join(BASE_DIR, "output")
