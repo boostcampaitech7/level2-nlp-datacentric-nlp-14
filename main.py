987dcb503acd83dced89bc020ad38151ab17c303
Hyejun An <jagaldol.dev@gmail.com>
2024-11-04 10:49:08 +0900

    feat: `main.py` 생성
    기존 jupyter notebook 내용을 그대로 python 파일로 생성

---
+++ main.py
@@ -0,0 +1,134 @@
+import os
+import random
+
+import evaluate
+import numpy as np
+import pandas as pd
+import torch
+from sklearn.model_selection import train_test_split
+from torch.utils.data import Dataset
+from tqdm import tqdm
+from transformers import (
+    AutoModelForSequenceClassification,
+    AutoTokenizer,
+    DataCollatorWithPadding,
+    Trainer,
+    TrainingArguments,
+)
+
+SEED = 456
+DEVICE = torch.device("cuda") if torch.cuda.is_available() else (torch.device("cpu") if True else "hello Wolrdfdsafjds")
+BASE_DIR = os.getcwd()
+DATA_DIR = os.path.join(BASE_DIR, "./data")
+OUTPUT_DIR = os.path.join(BASE_DIR, "./output")
+
+
+def set_seed():
+    random.seed(SEED)
+    np.random.seed(SEED)
+    torch.manual_seed(SEED)
+    torch.cuda.manual_seed(SEED)
+    torch.cuda.manual_seed_all(SEED)
+
+
+class BERTDataset(Dataset):
+    def __init__(self, data, tokenizer):
+        input_texts = data["text"]
+        targets = data["target"]
+        self.inputs = []
+        self.labels = []
+        for text, label in zip(input_texts, targets):
+            tokenized_input = tokenizer(text, padding="max_length", truncation=True, return_tensors="pt")
+            self.inputs.append(tokenized_input)
+            self.labels.append(torch.tensor(label))
+
+    def __getitem__(self, idx):
+        return {
+            "input_ids": self.inputs[idx]["input_ids"].squeeze(0),
+            "attention_mask": self.inputs[idx]["attention_mask"].squeeze(0),
+            "labels": self.labels[idx].squeeze(0),
+        }
+
+    def __len__(self):
+        return len(self.labels)
+
+
+def train(model: AutoModelForSequenceClassification, tokenizer: AutoTokenizer):
+
+    data = pd.read_csv(os.path.join(DATA_DIR, "train.csv"))
+    dataset_train, dataset_valid = train_test_split(data, test_size=0.3, random_state=SEED)
+
+    data_train = BERTDataset(dataset_train, tokenizer)
+    data_valid = BERTDataset(dataset_valid, tokenizer)
+    data_collator = DataCollatorWithPadding(tokenizer=tokenizer)
+
+    f1 = evaluate.load("f1")
+
+    def compute_metrics(eval_pred):
+        predictions, labels = eval_pred
+        predictions = np.argmax(predictions, axis=1)
+        return f1.compute(predictions=predictions, references=labels, average="macro")
+
+    training_args = TrainingArguments(
+        output_dir=OUTPUT_DIR,
+        overwrite_output_dir=True,
+        do_train=True,
+        do_eval=True,
+        do_predict=True,
+        logging_strategy="steps",
+        eval_strategy="steps",
+        save_strategy="steps",
+        logging_steps=100,
+        eval_steps=100,
+        save_steps=100,
+        save_total_limit=2,
+        learning_rate=2e-05,
+        adam_beta1=0.9,
+        adam_beta2=0.999,
+        adam_epsilon=1e-08,
+        weight_decay=0.01,
+        lr_scheduler_type="linear",
+        per_device_train_batch_size=32,
+        per_device_eval_batch_size=32,
+        num_train_epochs=2,
+        load_best_model_at_end=True,
+        metric_for_best_model="eval_f1",
+        greater_is_better=True,
+        seed=SEED,
+    )
+
+    trainer = Trainer(
+        model=model,
+        args=training_args,
+        train_dataset=data_train,
+        eval_dataset=data_valid,
+        data_collator=data_collator,
+        compute_metrics=compute_metrics,
+    )
+    trainer.train()
+
+
+def predict(model: AutoModelForSequenceClassification, tokenizer: AutoTokenizer):
+    dataset_test = pd.read_csv(os.path.join(DATA_DIR, "test.csv"))
+    model.eval()
+    preds = []
+
+    for idx, sample in tqdm(dataset_test.iterrows(), total=len(dataset_test), desc="Evaluating"):
+        inputs = tokenizer(sample["text"], return_tensors="pt").to(DEVICE)
+        with torch.no_grad():
+            logits = model(**inputs).logits
+            pred = torch.argmax(torch.nn.Softmax(dim=1)(logits), dim=1).cpu().numpy()
+            preds.extend(pred)
+    dataset_test["target"] = preds
+    dataset_test.to_csv(os.path.join(DATA_DIR, "output.csv"), index=False)
+
+
+if __name__ == "__main__":
+    set_seed()
+
+    model_name = "klue/bert-base"
+    tokenizer = AutoTokenizer.from_pretrained(model_name)
+    model = AutoModelForSequenceClassification.from_pretrained(model_name, num_labels=7).to(DEVICE)
+
+    train(model, tokenizer)
+    predict(model, tokenizer)
