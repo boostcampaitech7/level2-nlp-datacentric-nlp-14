612878a54a91c17a0ca3aca1116ecdabe4bffe09
Hyejun An <jagaldol.dev@gmail.com>
2024-11-05 16:38:51 +0900

    test: noise_labeling 파이프라인 추가

--- label_correction.py
+++ label_correction.py
@@ -4,6 +4,7 @@ import pandas as pd
 
 from configs import DATA_DIR
 from main import main
+from noise_data_filter import noise_labeling
 from utils import set_seed
 
 
@@ -16,6 +17,14 @@ def correct_label_errors(data: pd.DataFrame):
 if __name__ == "__main__":
     set_seed()
     data = pd.read_csv(os.path.join(DATA_DIR, "train.csv"))
-    corrected_data = correct_label_errors(data)
+
+    labeled_data = noise_labeling(data)
+
+    # TODO: restored_sentence 만드는 함수로 교체 필요
+    restored = pd.read_csv(os.path.join(DATA_DIR, "restored_sentences.csv"))
+    restored_data = pd.merge(labeled_data, restored[["ID", "restored"]], on="ID")
+    # TODO End
+
+    corrected_data = correct_label_errors(restored_data)
     corrected_data.to_csv(os.path.join(DATA_DIR, "label_corrected_train.csv"), index=False)
     main(corrected_data, do_predict=False)
